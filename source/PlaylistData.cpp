ac4f9da0b3706b3872847d8283ac766fbf9b6f67
jruaaa <joeyruan@zoho.com>
2020-10-27 12:19:21 -0700

    fixed gc1/2/3 and ssl

--- source/PlaylistData.cpp
+++ source/PlaylistData.cpp
@@ -182,7 +182,6 @@ std::map<Playlist, PlaylistData&> playlistMMRDatabase = {
     {Playlist::ONES, Ones},
     {Playlist::TWOS, Twos},
     {Playlist::THREES, Threes},
-    {Playlist::SOLO, Solo},
     {Playlist::HOOPS, Hoops},
     {Playlist::RUMBLE, Rumble},
     {Playlist::DROPSHOT, Dropshot},
@@ -192,24 +191,27 @@ std::map<Playlist, PlaylistData&> playlistMMRDatabase = {
 
 std::map<Rank, RankInfo> RankInfoDB
 {
-    { Rank::Unranked   ,{{ 133, 133, 133 }, "Unranked",}},
-    { Rank::Bronze1    ,{{ 227, 151, 68  }, "Bronze 1",}},
-    { Rank::Bronze2    ,{{ 227, 151, 68  }, "Bronze 2",}},
-    { Rank::Bronze3    ,{{ 227, 151, 68  }, "Bronze 3",}},
-    { Rank::Silver1    ,{{ 133, 133, 133 }, "Silver 1",}},
-    { Rank::Silver2    ,{{ 133, 133, 133 }, "Silver 2",}},
-    { Rank::Silver3    ,{{ 133, 133, 133 }, "Silver 3",}},
-    { Rank::Gold1      ,{{ 202, 149, 31  }, "Gold 1",}},
-    { Rank::Gold2      ,{{ 202, 149, 31  }, "Gold 2",}},
-    { Rank::Gold3      ,{{ 202, 149, 31  }, "Gold 3",}},
-    { Rank::Platinum1  ,{{ 50, 204, 252  }, "Platinum 1",}},
-    { Rank::Platinum2  ,{{ 50, 204, 252  }, "Platinum 2",}},
-    { Rank::Platinum3  ,{{ 50, 204, 252  }, "Platinum 3",}},
-    { Rank::Diamond1   ,{{ 2, 188, 255   }, "Diamond 1",}},
-    { Rank::Diamond2   ,{{ 2, 188, 255   }, "Diamond 2",}},
-    { Rank::Diamond3   ,{{ 2, 188, 255   }, "Diamond 3",}},
-    { Rank::Champ1     ,{{ 202, 137, 255 }, "Champion 1",}},
-    { Rank::Champ2     ,{{ 202, 137, 255 }, "Champion 2",}},
-    { Rank::Champ3     ,{{ 202, 137, 255 }, "Champion 3",}},
-    { Rank::GrandChamp ,{{ 244, 56, 236  }, "Grand Champion"}}
+    { Rank::Unranked         ,{{ 133, 133, 133 }, "Unranked",}},
+    { Rank::Bronze1          ,{{ 227, 151, 68  }, "Bronze 1",}},
+    { Rank::Bronze2          ,{{ 227, 151, 68  }, "Bronze 2",}},
+    { Rank::Bronze3          ,{{ 227, 151, 68  }, "Bronze 3",}},
+    { Rank::Silver1          ,{{ 133, 133, 133 }, "Silver 1",}},
+    { Rank::Silver2          ,{{ 133, 133, 133 }, "Silver 2",}},
+    { Rank::Silver3          ,{{ 133, 133, 133 }, "Silver 3",}},
+    { Rank::Gold1            ,{{ 202, 149, 31  }, "Gold 1",}},
+    { Rank::Gold2            ,{{ 202, 149, 31  }, "Gold 2",}},
+    { Rank::Gold3            ,{{ 202, 149, 31  }, "Gold 3",}},
+    { Rank::Platinum1        ,{{ 50, 204, 252  }, "Platinum 1",}},
+    { Rank::Platinum2        ,{{ 50, 204, 252  }, "Platinum 2",}},
+    { Rank::Platinum3        ,{{ 50, 204, 252  }, "Platinum 3",}},
+    { Rank::Diamond1         ,{{ 2, 188, 255   }, "Diamond 1",}},
+    { Rank::Diamond2         ,{{ 2, 188, 255   }, "Diamond 2",}},
+    { Rank::Diamond3         ,{{ 2, 188, 255   }, "Diamond 3",}},
+    { Rank::Champ1           ,{{ 202, 137, 255 }, "Champion 1",}},
+    { Rank::Champ2           ,{{ 202, 137, 255 }, "Champion 2",}},
+    { Rank::Champ3           ,{{ 202, 137, 255 }, "Champion 3",}},
+    { Rank::GrandChamp1      ,{{ 244, 47, 48   }, "Grand Champion 1"}}
+    { Rank::GrandChamp2      ,{{ 244, 47, 48   }, "Grand Champion 2"}}
+    { Rank::GrandChamp3      ,{{ 244, 47, 48   }, "Grand Champion 3"}}
+    { Rank::SupersonicLegend ,{{ 240, 240, 240 }, "Supersonic Legend"}}
 };
